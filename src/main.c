0e33ca7493e89b5aef2bdec6c85326e0f5d61e39
Rémi Cohen-Scali <remi.cohen-scali@renault.com>
2025-01-28 12:16:58 +0100

    Added UsersGuide.md: Start documentation

---
+++ src/main.c
@@ -0,0 +1,9 @@
+//
+// Created by rcoscali on 28/01/25.
+//
+
+#include "main.h"
+
+int main()
+{
+}
---
+++ src/main.h
@@ -0,0 +1,8 @@
+//
+// Created by rcoscali on 28/01/25.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif //MAIN_H
